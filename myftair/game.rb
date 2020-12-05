0c10e05d05566abb4b333e0d2781fe33e6985d45
errinlarsen <errinlarsen@gmail.com>
2020-12-04 20:07:04 -0700

    [🍐1] Add World and Room

--- myftair/game.rb
+++ myftair/game.rb
@@ -39,13 +39,11 @@ class Player
   end
 end
 
-
 # Item
 #
 # This class holds the state of items the Player might collect
 #
 class Item
-
   def self.types
     [:potion, :sword]
   end
@@ -71,7 +69,6 @@ class Item
     "a shiny awesome #{@type.to_s}"
   end
 end
-
 
 # Monster
 #
@@ -109,5 +106,88 @@ class Monster
       player.hurt(@attack_power)
       puts "The monster hits you for #{@attack_power} points."
     end
+  end
+end
+
+# World
+#
+# The "world" is a fixed 10 x 10 grid.
+#
+# [
+#   [room 0,0], [room 0,1], [room 0,2], # ...
+#   [room 1,0], [room 1,1], [room 1,2], # ...
+#   [room 2,0], [room 2,1], [room 2,2], # ...
+#   [room 3,0], [room 3,1], [room 3,2], # ...
+#   # ...
+# ]
+#
+class World
+  def self.width
+    10
+  end
+
+  def self.height
+    10
+  end
+
+  def initialize
+    @rooms = Array.new(World.height, Array.new(World.width))
+  end
+
+  def move_entity_north(entity)
+    entity.y_coord -= 1 if entity.y_coord > 0
+  end
+
+  def move_entity_south(entity)
+    entity.y_coord += 1 if entity.y_coord < World.height - 1
+  end
+
+  def move_entity_east(entity)
+    entity.x_coord += 1 if entity.x_coord < World.width - 1
+  end
+
+  def move_entity_west(entity)
+    entity.x_coord -= 1 if entity.x_coord > 0
+  end
+
+  def get_room_of(entity)
+    @rooms[entity.x_coord][entity.y_coord] ||= Room.new
+  end
+end
+
+# Room
+#
+# The Room class is what populates the World's "cells"
+#
+class Room
+  attr_accessor :size, :content
+
+  def initialize
+    @content   = get_content
+    @size      = get_size
+    @adjective = get_adjective
+  end
+
+  def interact(player)
+    return unless @content
+    @content.interact(player)
+    @content = nil
+  end
+
+  def to_s
+    "You are in a #{@size} room. It is #{@adjective}."
+  end
+
+  private
+  def get_content
+    [Monster, Item].sample.new
+  end
+
+  def get_size
+    ["small", "medium", "large"].sample
+  end
+
+  def get_adjective
+    ["pretty", "ugly", "hideous"].sample
   end
 end
